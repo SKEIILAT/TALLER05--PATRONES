2f3636f91529cec0e4a602ca514867e859387b83
BrayanBriones <137122666+BrayanBriones@users.noreply.github.com>
2024-11-28 13:48:23 -0500

    Update VistaListaSimple.java

--- GestorDeTareas/src/main/java/com/mycompany/gestordetareas/VistaListaSimple.java
+++ GestorDeTareas/src/main/java/com/mycompany/gestordetareas/VistaListaSimple.java
@@ -1,5 +1,6 @@
 public class VistaListaSimple implements VistaStrategy{
-  void VisualizarTarea(Tarea tarea){
+  @Override
+  public void VisualizarTarea(Tarea tarea){
     System.out.println("Se visualiza la lista simple");
   }
 }
