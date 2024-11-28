83e2ad08ed8360d7b0d37c81faa4b3f0cecd3870
JairPalaguachi <jpalagua@espol.edu.ec>
2024-11-27 21:21:37 -0500

    FactoryMethod

---
+++ GestorDeTareas/src/main/java/com/mycompany/gestordetareas/Tarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestordetareas;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Tarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ GestorDeTareas/src/main/java/com/mycompany/gestordetareas/TareaSimple.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gestordetareas;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class TareaSimple extends Tarea {
+    
+    @Override
+    public  void visualizarTarea(Tarea t){
+        System.out.println("Visualizando tarea simple... ");
+    }
+    
+}
